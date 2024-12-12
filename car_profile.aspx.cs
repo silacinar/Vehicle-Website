c4ca6dda1746f9d5d93503430ba39b41bbc7b12d
KaganAlaybeyoglu <alaybeyoglukagan@gmail.com>
2024-12-12 17:31:22 +0300

    server

--- car_profile.aspx.cs
+++ car_profile.aspx.cs
@@ -19,7 +19,7 @@ namespace Comecar
             if (!IsPostBack) // Sayfa ilk kez yüklendiğinde çalışacak
             {
                 // Veritabanına bağlanarak vehicle verilerini çekme
-                string connectionString = "Server=DESKTOP-8D8OQ9R;Database=COMECAR;Integrated Security=True;";
+                string connectionString = "Server=DESKTOP-LI7EMTS;Database=COMECAR;Integrated Security=True;";
                 string query = @"
                     SELECT
                         DAMAGE,
