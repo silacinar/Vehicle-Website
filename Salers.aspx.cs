a09d9ac56bc031df0ddb0ab97332f890b75634c3
HakanAlaybeyoglu <alaybeyoglu11@gmail.com>
2024-12-18 17:01:59 +0300

    Revert "Update"
    This reverts commit f9dcfcb873d406fa3687ef268a8f5638ccad0f79.

--- Salers.aspx.cs
+++ Salers.aspx.cs
@@ -19,7 +19,7 @@ namespace Comecar
             {
                 // Veritabanına bağlanarak salers verilerini çekme
 
-                string connectionString = "Server=DESKTOP-8D8OQ9R;Database=COMECAR;Integrated Security=True;";
+                string connectionString = "Server=HP\\SQLEXPRESS;Database=COMECAR;Integrated Security=True;";
 
                 string query = @"
                     SELECT * FROM SALERS";
